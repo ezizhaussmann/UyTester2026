37ce0c72e001af33a70801054a4deb04d0afc311
haussmann <aziznehal1@gmail.com>
2026-04-15 22:00:00 +0200

    my logger

---
+++ src/main/java/org/tester/xxx_Play/mesTestsAutomatiques.java
@@ -0,0 +1,18 @@
+package org.tester.xxx_Play;
+
+import com.microsoft.playwright.Page;
+import com.microsoft.playwright.options.AriaRole;
+import org.junit.Test;
+
+/**
+ * @created : 26/03/2026,01:51,jeu.
+ **/
+public class mesTestsAutomatiques {
+    // Note : Décommente les @Test si tu as JUnit configuré
+
+    // @Test
+    // public void testGoogle() {
+    //    page.navigate("https://www.google.com");
+    //    page.getByRole(AriaRole.BUTTON, new Page.GetByRoleOptions().setName("Tout accepter")).click();
+    // }
+}
